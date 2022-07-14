88759f57f93429d4fa90451604c690641b4395a9
k111ua <guiolder@hotmail.com>
2022-07-14 18:20:44 +0800

    update

--- src/views/login/index copy.vue
+++ src/views/login/index copy.vue
@@ -5,25 +5,24 @@
       <el-tab-pane label="注册"></el-tab-pane>
     </el-tabs>
     <el-form
-      ref="ruleFormRef"
-      :model="ruleForm"
-      :rules="rules"
-      labelPosition="top"
-      class="demo-ruleForm"
-      :size="formSize"
+      ref="loginForm"
+      :model="ruleLogin"
       status-icon
+      :rules="rules"
+      class="login-form"
+      label-position="top"
     >
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="ruleForm.username" />
+        <el-input type="text" :model="ruleLogin.username" />
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="ruleForm.password" type="password" showPassword />
+        <el-input type="password" :model="ruleLogin.password" show-password />
       </el-form-item>
       <el-form-item label="重复密码" prop="rePassword">
-        <el-input v-model="ruleForm.rePassword" type="password" showPassword />
+        <el-input type="password" :model="ruleLogin.rePassword" show-password />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">Create</el-button>
+        <el-button type="primary">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -31,34 +30,37 @@
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import type { FormInstance, FormRules, TabsPaneContext } from 'element-plus'
+import type { FormInstance } from 'element-plus'
+import type { TabsPaneContext } from 'element-plus'
 
 const tabClick = (tab: TabsPaneContext) => {
   console.log(tab.props.label)
 }
+const loginForm = ref<FormInstance>()
 const tab = ref('登录')
-const formSize = ref('default')
-const ruleFormRef = ref<FormInstance>()
-const ruleForm = reactive({
+
+const ruleLogin = reactive({
   username: '',
   password: '',
   rePassword: ''
 })
 
-const rules = reactive<FormRules>({
+console.log(ruleLogin.username)
+
+const rules = reactive({
   username: [
     { required: true, message: 'Please input Activity name', trigger: 'blur' }
   ],
   password: [
-    { required: true, message: 'Please input activity form', trigger: 'blur' }
+    { required: true, message: 'Please input Activity name', trigger: 'blur' }
   ],
   rePassword: [
-    { required: true, message: 'Please input activity form', trigger: 'blur' }
+    { required: true, message: 'Please input Activity name', trigger: 'blur' }
   ]
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .login-panel {
   width: 400px;
   margin: 200px auto 0 auto;
